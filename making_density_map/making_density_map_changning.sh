1bf8d2e41061076856cf2f072642e85dfacd9245
EnzeZHANG <zhangenze@link.cuhk.edu.hk>
2021-07-09 16:17:51 +0800

    update

--- making_density_map/making_density_map_changning.sh
+++ making_density_map/making_density_map_changning.sh
@@ -1,9 +1,15 @@
-#python making_density_map.py --input /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/lola_date_hour_filter.txt --output /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/density_map_new --minlo 104.2 --maxlo 105.4 --minla 27.8 --maxla 28.6
-
+#bash making_density_map_changning.sh ../catalog/lola_date_hour_filter_changning.txt ../density_map_changning
 
 input=$1
 output=$2
+
+
+if [ -d $output ];then
+    echo "$output exist"
+else
+    mkdir $output
+fi
+
 python making_density_map.py --input $input --output $output --minlo 104.2 --maxlo 105.4 --minla 27.8 --maxla 28.6 
 
-#python making_density_map.py --input /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/test_post_processing/final_out_lola_resnet18_SGD_momentum_0_0.001_seed1_weight_decay_0.01_1-3_droprate_add_mv_aug_H5-25_V5-10_Feb23inference_pairs_changning_5day.txt --output /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/post_process_map_resnet18_SGD_momentum_0_0.001_seed1_weight_decay_0.01_1-3_droprate_add_mv_aug_H5-25_V5-10_Feb23inference_pairs_changning_5day --minlo 104.2 --maxlo 105.4 --minla 27.8 --maxla 28.6
-#python making_density_map.py --input /home/zez/data2/identify_earthquake_distribution_change/weiyuan/new_data/all_data/date_lola_mag_all.txt --output /home/zez/data2/identify_earthquake_distribution_change/weiyuan/new_data/all_data/density_map --minlo 104.21 --maxlo 105 --minla 29.2 --maxla 29.8
+bash batch_change_projection_changning.sh $output
--- making_density_map/making_density_map_weiyuan.sh
+++ making_density_map/making_density_map_weiyuan.sh
@@ -1,5 +1,13 @@
-#python making_density_map.py --input /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/lola_date_hour_filter.txt --output /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/density_map_new --minlo 104.2 --maxlo 105.4 --minla 27.8 --maxla 28.6
-#python making_density_map.py --input /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/test_post_processing/final_out_lola_resnet18_SGD_momentum_0_0.001_seed1_weight_decay_0.01_1-3_droprate_add_mv_aug_H5-25_V5-10_Feb23inference_pairs_changning_5day.txt --output /home/zez/data2/identify_earthquake_distribution_change/data_MentLY/post_process_map_resnet18_SGD_momentum_0_0.001_seed1_weight_decay_0.01_1-3_droprate_add_mv_aug_H5-25_V5-10_Feb23inference_pairs_changning_5day --minlo 104.2 --maxlo 105.4 --minla 27.8 --maxla 28.6
+
+#bash making_density_map_weiyuan.sh ../catalog/date_lola_hour_mag_weiyuan_all.txt ../density_map_weiyuan
 catalog=$1
 output=$2
+
+if [ -d $output ];then
+    echo "$output exist"
+else
+    mkdir $output
+fi
 python making_density_map_hourly.py --input $catalog --output $output --resolution 6 --minlo 104.21 --maxlo 105 --minla 29.2 --maxla 29.8
+
+bash batch_change_projection_weiyuan.sh $output
