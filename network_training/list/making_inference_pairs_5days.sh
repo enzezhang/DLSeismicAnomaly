bc7943402915e7a402accc6daec39fee7ee61268
EnzeZHANG <zhangenze@link.cuhk.edu.hk>
2021-08-23 18:50:43 -0500

    update

--- network_training/list/making_inference_pairs_5days.sh
+++ network_training/list/making_inference_pairs_5days.sh
@@ -7,9 +7,9 @@ data_path=$(python2 ${para_py} -p ${para_file} data_weiyuan)
 work_root=$(python2 ${para_py} -p ${para_file} working_root)
 
 find ${data_path}/*_lola.tif > image.txt
-python ${work_root}/making_pairs_inference_5days.py --image image.txt > inference_weiyuan_6_hour_5days.txt
+python ${work_root}/making_pairs_inference_5days.py --input image.txt > inference_weiyuan_6_hour_5days.txt
 
 data_path=$(python2 ${para_py} -p ${para_file} data_changning)
 
 find ${data_path}/*_lola.tif > image.txt
-python ${work_root}/making_pairs_inference_5days.py --image image.txt > inference_changning_daily_5days.txt
+python ${work_root}/making_pairs_inference_5days.py --input image.txt > inference_changning_daily_5days.txt
